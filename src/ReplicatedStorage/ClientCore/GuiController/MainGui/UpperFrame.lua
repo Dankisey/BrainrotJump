ef2b4fd070895cc56f818f37b58298cb16228de2
vn3x <46728296+lapollinaria@users.noreply.github.com>
2025-09-25 14:04:25 +0300

    Spins button added to upper HUD

--- src/ReplicatedStorage/ClientCore/GuiController/MainGui/UpperFrame.lua
+++ src/ReplicatedStorage/ClientCore/GuiController/MainGui/UpperFrame.lua
@@ -7,6 +7,14 @@ function UpperFrame:AfterPlayerLoaded(player: Player)
     
 end
 
+function UpperFrame:Initialize()
+    self.SpinButton = self._frame.SpinButton
+
+    self._controllers.ButtonsInteractionsConnector:ConnectButton(self.SpinButton, function()
+        self._controllers.GuiController.SpinGui:Enable(true)
+    end)
+end
+
 function UpperFrame.new(frame: Frame)
     local self = setmetatable(UpperFrame, {__index = ControllerTemplate})
     self._frame = frame
