47759936ff683360e09821729461b020be81f0d6
Dankisey <91971084+Dankisey@users.noreply.github.com>
2025-09-26 17:22:02 +0300

    Update ConflictingGuisConfig.lua

--- src/ReplicatedStorage/Configs/ConflictingGuisConfig.lua
+++ src/ReplicatedStorage/Configs/ConflictingGuisConfig.lua
@@ -1,11 +1,21 @@
 local ConflictingGuisConfig = {
     ConfirmationPromptGui = true;
+    AchievementsGui = true;
+    GroupRewardsGui = true;
+    DailyRewardsGui = true;
+    TimeRewardsGui = true;
     RobuxShopGui = true;
     InventoryGui = true;
+    HatchingGui = true;
     UpgradesGui = true;
     FoodCapGui = true;
+    RebirthGui = true;
+    WorldsGui = true;
+    TrailsGui = true;
     WingsGui = true;
 	SacksGui = true;
+    SpinGui = true;
+    PetsGui = true;
 }
 
 return ConflictingGuisConfig
